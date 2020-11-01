dd9fd53b3c1ee3d92fce29875939b56a9242c0d5
eduardosoy <61882315+eduardosoy@users.noreply.github.com>
2020-11-01 14:31:26 +0100

    correcion de algunos errores (quedan busgs visuales) pantallas en horizontal flecha de vuelta hacia menu inicio

--- app/src/main/java/com/aplicacionps/Jugar.java
+++ app/src/main/java/com/aplicacionps/Jugar.java
@@ -2,13 +2,22 @@ package com.aplicacionps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class Jugar extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+    public void volver(View view){
+        Intent volver= new Intent (this, MainActivity.class);
+        startActivity(volver);
     }
 }
