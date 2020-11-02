b1e78f73663473a96796d8544095a2afa0da45f6
adrisierra <adriansr2010@gmail.com>
2020-11-02 16:49:23 +0100

    Merge branch 'Adrian' of C:\Users\Adrian\AndroidStudioProjects\CoronaStop with conflicts.

--- app/src/main/java/com/aplicacionps/Logros.java
+++ app/src/main/java/com/aplicacionps/Logros.java
@@ -2,13 +2,20 @@ package com.aplicacionps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class Logros extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logros);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+    public void volver(View view){
+        Intent volver= new Intent (this, MainActivity.class);
+        startActivity(volver);
     }
 }
