3a68edbde5892eeae93301b068f037eacd3bc5ef
flavia29 <flaviavasg@gmail.com>
2020-11-02 13:08:12 +0100

    camino autobus hasta antes de comprar

---
+++ app/src/main/java/com/aplicacionps/colaparapagar.java
@@ -0,0 +1,31 @@
+package com.aplicacionps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class colaparapagar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colaparapagar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+    //clase personaje...+%
+    public void efectivo(View view){
+        Intent efectivo= new Intent (this, colaparapagar.class);
+        startActivity(efectivo);
+    }
+
+    public void tarjeta(View view){
+        Intent tarjeta= new Intent (this, colaparapagar.class);
+        startActivity(tarjeta);
+    }
+}
---
+++ app/src/main/java/com/aplicacionps/superdentro.java
@@ -0,0 +1,29 @@
+package com.aplicacionps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class superdentro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_superdentro);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    public void guantesygel(View view){
+        Intent guantesygel= new Intent (this, colaparapagar.class);
+        startActivity(guantesygel);
+    }
+    //clase personaje...+%
+    public void nada(View view){
+        Intent nada= new Intent (this, colaparapagar.class);
+        startActivity(nada);
+    }
+}
