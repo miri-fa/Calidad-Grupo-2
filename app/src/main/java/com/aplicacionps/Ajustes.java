8e7509b7b1893634eebdd146a21f5f490e4155c8
JoseLuis <jolutoher18@gmail.com>
2020-11-02 20:40:23 +0100

    IMPLEMENTACIÓN MÚSICA DE FONDO (DEMO)

--- app/src/main/java/com/aplicacionps/Ajustes.java
+++ app/src/main/java/com/aplicacionps/Ajustes.java
@@ -1,7 +1,7 @@
 package com.aplicacionps;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
@@ -12,5 +12,20 @@ public class Ajustes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajustes);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Intent i = new Intent(this, AudioService.class);
+        i.putExtra("action", AudioService.PAUSE);
+        startService(i);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent i = new Intent(this, AudioService.class);
+        i.putExtra("action", AudioService.START);
+        startService(i);
     }
 }
--- app/src/main/java/com/aplicacionps/MainActivity.java
+++ app/src/main/java/com/aplicacionps/MainActivity.java
@@ -2,20 +2,13 @@ package com.aplicacionps;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
-
-
-    MediaPlayer click;
-    ImageButton boton_ajustes;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        boton_ajustes = (ImageButton)findViewById(R.id.imageButton12);
-        click = MediaPlayer.create(this, R.raw.sonido_click);
 
-        boton_ajustes.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                click.start();
-            }
-        });
     }
     public void jugar(View view){
         Intent jugar= new Intent (this, Jugar.class);
@@ -48,6 +33,21 @@ public class MainActivity extends AppCompatActivity {
     public void ajustes(View view){
         Intent sett = new Intent (this, Ajustes.class);
         startActivity(sett);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Intent i = new Intent(this, AudioService.class);
+        i.putExtra("action", AudioService.PAUSE);
+        startService(i);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent i = new Intent(this, AudioService.class);
+        i.putExtra("action", AudioService.START);
+        startService(i);
     }
 
 }
