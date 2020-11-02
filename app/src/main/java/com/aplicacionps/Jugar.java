204af774cc396fc22e8de45cf2d58c7cb689ed7a
flavia29 <flaviavasg@gmail.com>
2020-11-02 09:41:38 +0100

    toast quitados

--- app/src/main/java/com/aplicacionps/Jugar.java
+++ app/src/main/java/com/aplicacionps/Jugar.java
@@ -21,7 +21,6 @@ public class Jugar extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
         // La actividad está a punto de hacerse visible.
     }
     public void escenarioCasa(View view){
@@ -31,25 +30,21 @@ public class Jugar extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
         // La actividad se ha vuelto visible (ahora se "reanuda").
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
         // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
         // La actividad ya no es visible (ahora está "detenida")
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
         // La actividad está a punto de ser destruida.
     }
 }
