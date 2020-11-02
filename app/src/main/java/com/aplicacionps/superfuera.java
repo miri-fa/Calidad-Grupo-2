4f528e04560e4c3b511199b8052c735c8f67f76d
flavia29 <flaviavasg@gmail.com>
2020-11-02 13:07:59 +0100

    camino autobus hasta antes de comprar

--- app/src/main/java/com/aplicacionps/superfuera.java
+++ app/src/main/java/com/aplicacionps/superfuera.java
@@ -2,7 +2,11 @@ package com.aplicacionps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class superfuera extends AppCompatActivity {
 
@@ -10,6 +14,17 @@ public class superfuera extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_superfuera);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    //clase personaje...+%
+    public void entrar(View view){
+        Intent entrar= new Intent (this, superdentro.class);
+        startActivity(entrar);
+    }
+    public void esperar(View view){
+        Intent esperar= new Intent (this, superdentro.class);
+        startActivity(esperar);
     }
 
 }
