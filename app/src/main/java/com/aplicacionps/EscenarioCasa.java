1b7443e6e61e19d43fc9518cae2c08e9d0b4a06f
flavia29 <flaviavasg@gmail.com>
2020-11-02 09:58:58 +0100

    ENLAZADO CLASE CASA CON ELEGIRCAMINO

--- app/src/main/java/com/aplicacionps/EscenarioCasa.java
+++ app/src/main/java/com/aplicacionps/EscenarioCasa.java
@@ -22,6 +22,10 @@ public class EscenarioCasa extends AppCompatActivity {
         Intent caminoBueno = new Intent (this, elegirCamino.class);
         startActivity(caminoBueno);
     }
-
+    //Flata crear una clase personaje en la que al elegir el camino malo incremente el porcentaje de contagio
+    public void caminoMalo(View view){
+        Intent caminoMalo = new Intent (this, elegirCamino.class);
+        startActivity(caminoMalo);
+    }
 
 }
