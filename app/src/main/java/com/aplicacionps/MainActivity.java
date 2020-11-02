97ea6848dddebba2b989ecd379d8a79e48c396e7
adrisierra <adriansr2010@gmail.com>
2020-11-02 17:36:14 +0100

    Sonido click(boton_ajustes)

--- app/src/main/java/com/aplicacionps/MainActivity.java
+++ app/src/main/java/com/aplicacionps/MainActivity.java
@@ -2,15 +2,19 @@ package com.aplicacionps;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Intent jugar;
+    MediaPlayer click;
+    ImageButton boton_ajustes;
 
 
     @Override
@@ -19,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        boton_ajustes = (ImageButton)findViewById(R.id.imageButton12);
+        click = MediaPlayer.create(this, R.raw.sonido_click);
 
+        boton_ajustes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                click.start();
+            }
+        });
     }
     public void jugar(View view){
         Intent jugar= new Intent (this, Jugar.class);
