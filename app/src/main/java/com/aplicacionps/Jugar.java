4968e460937f6c6e88e8813161319777c13aa7c4
Alba-18 <albasevi8@gmail.com>
2020-11-09 00:28:58 +0100

    COMENTARIOS_E_ID_ESCENARIOS

--- app/src/main/java/com/aplicacionps/Jugar.java
+++ app/src/main/java/com/aplicacionps/Jugar.java
@@ -13,11 +13,15 @@ import android.widget.Toast;
 
 public class Jugar extends AppCompatActivity {
 
-
+    //Relacionamos la clase Jugar con su respectivo XML activity_jugar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar);
+
+        //Cambiamos la orientación para que la pantalla se pueda ver en horizontal y que
+        //se muestre a pantalla completa, sin barra de notificaciones
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
@@ -26,13 +30,33 @@ public class Jugar extends AppCompatActivity {
         super.onStart();
         // La actividad está a punto de hacerse visible.
     }
+
+    //Este método supermercado corresponde con la parte lógica del botón 'supermercado' del XML activity_jugar,
+    //y lo que hará es que nos llevará al siguiente escenario que corresponde la clase EscenarioCasa
     public void supermercado(View view){
         Intent supermercado = new Intent (this, EscenarioCasa.class);
         startActivity(supermercado);
     }
+
+    //Este método instituto corresponde con la parte lógica del botón 'Instituto' del XML activity_jugar,
+    //y por el momento, al no estar disponible la historia jugable Instituto nos saldrá por pantalla
+    //un toast con el mensaje 'No disponible'
     public void instituto(View view){
         Toast.makeText(this, "No disponible", Toast.LENGTH_SHORT).show();
     }
+
+    //Este método volver corresponde con la parte lógica del botón 'flecha_atras' del XML activity_jugar,
+    //y lo que hará es que nos llevará a la pantalla de inicio, correspondiente a la clase MainActivity
+
+    public void volver(View view){
+        Intent volver= new Intent (this, MainActivity.class);
+        startActivity(volver);
+
+    }
+
+    //Estos 2 últimos métodos sirven para implementar la música del juego dentro de esta clase y para las demás,
+    //siendo la música constante y permanente durante el tiempo que te encuentres dentro del juego
+
     @Override
     public void onPause() {
         super.onPause();
@@ -61,10 +85,5 @@ public class Jugar extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         // La actividad está a punto de ser destruida.
-    }
-    public void volver(View view){
-        Intent volver= new Intent (this, MainActivity.class);
-        startActivity(volver);
-
     }
 }
