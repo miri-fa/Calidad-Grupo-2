779968028f80b6e6566d3e9efbe726054569a047
mrjesuland <61882329+mrjesuland@users.noreply.github.com>
2020-11-08 16:54:04 +0100

    He añadido los comentarios a mis respectivas clases y los strings correspondientes en strings.xml

--- app/src/main/java/com/aplicacionps/DentroDeLaCalle.java
+++ app/src/main/java/com/aplicacionps/DentroDeLaCalle.java
@@ -12,22 +12,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DentroDeLaCalle extends AppCompatActivity {
 
+    //Relacionamos la clase DentroDeLaCalle con su respectivo XML activity_dentro_de_la_calle
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dentro_de_la_calle);
+
+        //Cambiamos la orientación para que la pantalla se pueda ver en horizontal y que
+        //se muestre a pantalla completa, sin barra de notificaciones
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
+    //Este método MeLaQuito corresponde con la parte lógica del botón 'Me la quito, necesito respirar' del XML activity_dentro_de_la_calle,
+    //y por el momento, al no estar disponible la acción de incrementar el porcentaje de contagio de nuestro personaje,
+    //nos saldrá por pantalla un toast con el mensaje 'No disponible'
 
     public void MeLaQuito(View view){
         Toast.makeText(this, "No disponible", Toast.LENGTH_SHORT).show();
     }
+
+    //Este método NoMeLaQuito corresponde con la parte lógica del botón 'No me la quito' del XML activity_dentro_de_la_calle,
+    //y lo que hará es que nos llevará al siguiente escenario que corresponde la clase superfuera
+
     public void NoMeLaQuito(View view){
         Intent NoMeLaQuito= new Intent(this, superfuera.class);
         startActivity(NoMeLaQuito);
     }
+
+    //Estos 2 últimos métodos sirven para implementar la música del juego dentro de esta clase y para las demás,
+    //siendo la música constante y permanente durante el tiempo que te encuentres dentro del juego
 
     @Override
     public void onPause() {
--- app/src/main/java/com/aplicacionps/irAndando.java
+++ app/src/main/java/com/aplicacionps/irAndando.java
@@ -12,13 +12,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class irAndando extends AppCompatActivity {
 
+    //Relacionamos la clase irAndando con su respectivo XML activity_ir_andando
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ir_andando);
+
+        //Cambiamos la orientación para que la pantalla se pueda ver en horizontal y que
+        //se muestre a pantalla completa, sin barra de notificaciones
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    //Este método CalleSinGente corresponde con la parte lógica del botón 'calle vacía, pero tardo más' del XML activity_ir_andando,
+    //y lo que hará es que nos llevará al siguiente escenario que corresponde la clase DentroDeLaCalle
 
     public void CalleSinGente(View view){
         Intent CalleSinGente= new Intent(this, DentroDeLaCalle.class);
@@ -26,9 +35,16 @@ public class irAndando extends AppCompatActivity {
 
     }
 
+    //Este método CalleConGente corresponde con la parte lógica del botón 'calle con gente' del XML activity_ir_andando,
+    //y por el momento, al no estar disponible la acción de incrementar el porcentaje de contagio de nuestro personaje,
+    //nos saldrá por pantalla un toast con el mensaje 'No disponible'
+
     public void CalleConGente(View view){
         Toast.makeText(this, "No disponible", Toast.LENGTH_SHORT).show();
     }
+
+    //Estos 2 últimos métodos sirven para implementar la música del juego dentro de esta clase y para las demás,
+    //siendo la música constante y permanente durante el tiempo que te encuentres dentro del juego
 
     @Override
     public void onPause() {
