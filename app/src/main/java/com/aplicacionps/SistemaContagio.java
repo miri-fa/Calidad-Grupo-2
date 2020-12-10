0b6bf8b380879f1ef0036993349327dd168a4290
flavia29 <61882314+flavia29@users.noreply.github.com>
2020-12-10 13:02:26 +0100

    Sistema contagio

---
+++ app/src/main/java/com/aplicacionps/SistemaContagio.java
@@ -0,0 +1,15 @@
+package com.aplicacionps;
+
+public class SistemaContagio {
+
+    private int porcentaje;
+
+    public SistemaContagio(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public int getPorcentaje(){
+        return porcentaje;
+    }
+
+}
