33d6ee3b02ceeb429514d67374d36b383bf9ce0a
Alba-18 <albasevi8@gmail.com>
2020-12-11 18:19:17 +0100

    nuevo_commit

---
+++ app/src/main/java/com/aplicacionps/comojugar4.java
@@ -0,0 +1,53 @@
+package com.aplicacionps;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class comojugar4 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comojugar4);
+        //Cambiamos la orientación para que la pantalla se pueda ver en horizontal y que
+        //se muestre a pantalla completa, sin barra de notificaciones
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    //Cambiamos la orientación para que la pantalla se pueda ver en horizontal y que
+    //se muestre a pantalla completa, sin barra de notificaciones
+
+    //Metodo para poder volver a la pantalla inicio
+    public void volver(View view){
+        Intent volver= new Intent (this, MainActivity.class);
+        startActivity(volver);
+
+    }
+
+    //Estos 2 últimos métodos sirven para implementar la música del juego dentro de esta clase y para las demás,
+    //siendo la música constante y permanente durante el tiempo que te encuentres dentro del juego
+    @Override
+    public void onPause() {
+        super.onPause();
+        Intent i = new Intent(this, AudioService.class);
+        i.putExtra("action", AudioService.PAUSE);
+        startService(i);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences("save", MODE_PRIVATE);
+        Boolean valordelboton = sharedPreferences.getBoolean("value", false);
+        if (valordelboton != true) {
+            Intent i = new Intent(this, AudioService.class);
+            i.putExtra("action", AudioService.START);
+            startService(i);
+        }
+    }
+}
