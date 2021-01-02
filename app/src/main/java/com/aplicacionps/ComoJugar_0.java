0ced943932dee86db4cbf8d576cf5a4d696a5e50
Alba-18 <albasevi8@gmail.com>
2021-01-02 12:30:41 +0100

    commit_partealba

--- app/src/main/java/com/aplicacionps/ComoJugar_0.java
+++ app/src/main/java/com/aplicacionps/ComoJugar_0.java
@@ -14,34 +14,39 @@ import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link tutorial2#newInstance} factory method to
+ * Use the {@link ComoJugar_0#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class tutorial2 extends Fragment {
-    public tutorial2() {
+public class ComoJugar_0 extends Fragment {
+    public ComoJugar_0() {
         // Required empty public constructor
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    //Relacionamos la clase ComoJugar_0 con su respectivo XML fragment_ComoJugar_00
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tutorial2, container, false);
+        return inflater.inflate(R.layout.fragment_comojugar_1, container, false);
     }
+
+    //implementacion de boton que lleva de un fragmento a otro fragmento (ComoJugar_1)
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ImageButton boton_comojugar3 = view.findViewById(R.id.boton_comojugar3);
-        //implementacion de boton que lleva de un fragmento a otro fragment
-        boton_comojugar3.setOnClickListener(new View.OnClickListener() {
+        ImageButton btnsig = view.findViewById(R.id.boton_comosejuega1);
+        btnsig.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.tutorial3);
+            public void onClick(View v){
+                Navigation.findNavController(v).navigate(R.id.ComoJugar_1);
             }
         });
+
     }
 }
